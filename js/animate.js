1d2daddba94398e893d385a630aee415232a0651
Spec Ussa <115887844+Specussa@users.noreply.github.com>
2024-05-31 18:06:17 +0300

    fix css

--- js/animate.js
+++ js/animate.js
@@ -1,21 +1,3 @@
-const tournamentitem = document.querySelector('.tournament__item');
-const tournamentitems = document.querySelectorAll('.tournament__item');
-if(tournamentitem){
-  function onEntry(entry) {entry.forEach(change => {
-    if(change.isIntersecting){
-      change.target.classList.add('animated');
-      change.target.classList.add('visible');
-    }
-     else {
-      change.target.classList.remove('visible');
-    }
-  });};
-
-  let tournamentitemsopt = {threshold: [0.7]};
-  let tournamentitemsserv = new IntersectionObserver(onEntry, tournamentitemsopt);
-  for (let elm of tournamentitems) {tournamentitemsserv.observe(elm);}
-}
-
 const animateimage = document.querySelector('.animate_image');
 const animateimages = document.querySelectorAll('.animate_image');
 if(animateimage){
