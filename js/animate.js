4839c1f1fe4597f5e326e3edab3777f11938fb8e
Spec Ussa <115887844+Specussa@users.noreply.github.com>
2024-05-31 14:36:36 +0300

    add section tournament

---
+++ js/animate.js
@@ -0,0 +1,17 @@
+const tournamentitem = document.querySelector('.tournament__item');
+const tournamentitems = document.querySelectorAll('.tournament__item');
+if(tournamentitem){
+  function onEntry(entry) {entry.forEach(change => {
+    if(change.isIntersecting){
+      change.target.classList.add('animated');
+      change.target.classList.add('visible');
+    }
+     else {
+      change.target.classList.remove('visible');
+    }
+  });};
+
+  let tournamentitemsopt = {threshold: [0.5]};
+  let tournamentitemsserv = new IntersectionObserver(onEntry, tournamentitemsopt);
+  for (let elm of tournamentitems) {tournamentitemsserv.observe(elm);}
+}
